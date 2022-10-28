ce261e08d80cb9d90be0bb55e9ebf22e46dcb1f6
Dalton Miller <dalton.kye.miller@gmail.com>
2022-10-28 17:41:03 -0500

    added cool neon heading

--- src/components/invoice/InvoiceBuilder.tsx
+++ src/components/invoice/InvoiceBuilder.tsx
@@ -5,10 +5,10 @@ import { Button, Card, Input, Select, Textarea } from 'react-daisyui';
 import { trpc } from '../../utils/trpc';
 import { IoMdAdd } from 'react-icons/io';
 import { useRef, useState } from 'react';
-import { useAutoAnimate } from '@formkit/auto-animate/react';
 import ServiceBuilder from '../ServiceBuilder';
 import { useFloating } from '@floating-ui/react-dom';
 import { mergeRefs } from '../../utils/mergeRefs';
+import { AnimatePresence, motion } from 'framer-motion';
 
 export default function InvoiceBuilder() {
     const {
@@ -62,9 +62,6 @@ export function AddCompanyPanel() {
         formState: { errors },
     } = useForm({ resolver: zodResolver(createCompanySchema) });
     const [addCompany, setAddCompany] = useState(false);
-    const [parent] = useAutoAnimate<HTMLDivElement>({
-        duration: 100,
-    });
 
     const utils = trpc.useContext();
     const mutation = trpc.company.createCompany.useMutation({
@@ -79,7 +76,7 @@ export function AddCompanyPanel() {
     });
 
     return (
-        <div ref={mergeRefs([parent, reference])}>
+        <div ref={reference}>
             <Button
                 variant="outline"
                 onClick={() => setAddCompany(!addCompany)}
@@ -92,64 +89,74 @@ export function AddCompanyPanel() {
                     } transition-all`}
                 />
             </Button>
-            {addCompany && (
-                <Card
-                    className="glow-border absolute z-20 flex w-[500px] flex-col gap-2 bg-base-200"
-                    style={{
-                        position: strategy,
-                        top: y ?? 0,
-                        left: x ?? 0,
-                    }}
-                    ref={floating}
-                >
-                    <Card.Body>
-                        <Input
-                            placeholder={'Company Name...'}
-                            className={`${
-                                errors.name ? '!border-red-500' : ''
-                            }`}
-                            {...register('name')}
-                        />
-                        <Input
-                            placeholder={'Company Address...'}
-                            className={`${
-                                errors.address ? '!border-red-500' : ''
-                            }`}
-                            {...register('address')}
-                        />
-                        <Input
-                            placeholder={'Company City...'}
-                            className={`${
-                                errors.city ? '!border-red-500' : ''
-                            }`}
-                            {...register('city')}
-                        />
-                        <Textarea
-                            {...register('notes')}
-                            className={`${
-                                errors.notes ? '!border-red-500' : ''
-                            }`}
-                            placeholder="Notes..."
-                        ></Textarea>
-
-                        <Button
-                            variant="outline"
-                            onClick={handleSubmit(
-                                ({ name, address, city, notes }) => {
-                                    mutation.mutate({
-                                        name,
-                                        address,
-                                        city,
-                                        notes,
-                                    });
-                                }
-                            )}
+            <AnimatePresence>
+                {addCompany && (
+                    <motion.div
+                        className="absolute z-10 "
+                        initial={{ opacity: 0, scale: 0.5 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0 }}
+                        transition={{ duration: 0.15 }}
+                    >
+                        <Card
+                            className="glow-border flex w-[500px] flex-col gap-2 bg-base-200"
+                            style={{
+                                position: strategy,
+                                top: y ?? 0,
+                                left: x ?? 0,
+                            }}
+                            ref={floating}
                         >
-                            Create!
-                        </Button>
-                    </Card.Body>
-                </Card>
-            )}
+                            <Card.Body>
+                                <Input
+                                    placeholder={'Company Name...'}
+                                    className={`${
+                                        errors.name ? '!border-red-500' : ''
+                                    }`}
+                                    {...register('name')}
+                                />
+                                <Input
+                                    placeholder={'Company Address...'}
+                                    className={`${
+                                        errors.address ? '!border-red-500' : ''
+                                    }`}
+                                    {...register('address')}
+                                />
+                                <Input
+                                    placeholder={'Company City...'}
+                                    className={`${
+                                        errors.city ? '!border-red-500' : ''
+                                    }`}
+                                    {...register('city')}
+                                />
+                                <Textarea
+                                    {...register('notes')}
+                                    className={`${
+                                        errors.notes ? '!border-red-500' : ''
+                                    }`}
+                                    placeholder="Notes..."
+                                ></Textarea>
+
+                                <Button
+                                    variant="outline"
+                                    onClick={handleSubmit(
+                                        ({ name, address, city, notes }) => {
+                                            mutation.mutate({
+                                                name,
+                                                address,
+                                                city,
+                                                notes,
+                                            });
+                                        }
+                                    )}
+                                >
+                                    Create!
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </motion.div>
+                )}
+            </AnimatePresence>
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import InvoiceBuilder from '../components/invoice/InvoiceBuilder';
 import InvoiceRenderer from '../components/invoice/InvoiceRenderer';
+import { motion } from 'framer-motion';
 
 const Home: NextPage = () => {
     return (
@@ -11,12 +12,35 @@ const Home: NextPage = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className="flex min-h-screen gap-3 p-4">
-                <div className="h-full basis-1/3">
-                    <InvoiceBuilder />
-                </div>
-                <div className="glow-border min-h-full basis-2/3">
-                    <InvoiceRenderer />
+            <main className="flex min-h-screen flex-col items-center gap-3 p-4">
+                <motion.h1 className="text-9xl font-bold">
+                    {'INVOICE GENERATOR'.split('').map((l, k) => (
+                        <motion.span
+                            key={k}
+                            animate={{
+                                filter: [
+                                    'brightness(100%)',
+                                    'brightness(250%)',
+                                ],
+                            }}
+                            transition={{
+                                duration: 0.25,
+                                repeat: Infinity,
+                                repeatType: 'mirror',
+                                delay: k * 0.1,
+                            }}
+                        >
+                            {l}
+                        </motion.span>
+                    ))}
+                </motion.h1>
+                <div className="flex w-full gap-3">
+                    <div className="h-full basis-1/3">
+                        <InvoiceBuilder />
+                    </div>
+                    <div className="glow-border min-h-full basis-2/3">
+                        <InvoiceRenderer />
+                    </div>
                 </div>
             </main>
         </>
